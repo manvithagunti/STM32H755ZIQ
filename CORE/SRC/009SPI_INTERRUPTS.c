0f9f9503b22d4746cf338853451c5d8e4e4ffc83
manvithagunti <166311567+manvithagunti@users.noreply.github.com>
2025-10-28 10:14:11 +0530

    Update 009SPI_INTERRUPTS.c

--- CORE/SRC/009SPI_INTERRUPTS.c
+++ CORE/SRC/009SPI_INTERRUPTS.c
@@ -16,15 +16,17 @@
 #include<string.h>
 #include "stm32h7xx.h"
 
+//extern void initialise_monitor_handles(void);
 
-SPI_Handle_t SPI4handle;
 
-#define MAX_LEN 500
+SPI_Handle_t SPI1handle;
 
-char RcvBuff[MAX_LEN];
+#define MAX_LEN 100
 
-volatile char ReadByte;
+char RcvBuff[MAX_LEN]={1,1,11,1,1,1,1,1,1,1,11,1,1,1,1,1,1,1};
 
+uint8_t ReadByte;
+uint32_t i = 0,count=0,ready=0;
 
 volatile uint8_t rcvStop = 0;
 
@@ -33,78 +35,79 @@ volatile uint8_t dataAvailable = 0;
 
 void delay(void)
 {
-	for(uint32_t i = 0 ; i < 500000/2 ; i ++);
+	for(uint32_t i = 0 ; i < 500000 ; i ++);
 }
 
-/*
- * PB14 --> SPI2_MISO
- * PB15 --> SPI2_MOSI
- * PB13 -> SPI2_SCLK
- * PB12 --> SPI2_NSS
- * ALT function mode : 5
- */
 
-void SPI4_GPIOInits(void)
+//PA7-SPI1_MOSI
+//PA5-SPI1_SCK
+//PA4-SPI1_NSS
+//PA6-SPI1_MISO
+
+void SPI1_GPIOInits(void)
 {
 	GPIO_Handle_t SPIPins;
 
-	SPIPins.pGPIOx = GPIOE;
+	SPIPins.pGPIOx = GPIOA;
 	SPIPins.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_ALTFN;
 	SPIPins.GPIO_PinConfig.GPIO_PinAltFunMode =5;
 	SPIPins.GPIO_PinConfig.GPIO_PinOPType= GPIO_OP_TYPE_PP;
 	SPIPins.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
 	SPIPins.GPIO_PinConfig.GPIO_PinSpeed= GPIO_SPEED_LOW;
 
-	GPIOE_PCLK_EN();
+	GPIOA_PCLK_EN();
 
 	//CONFIGURING SCK
-	SPIPins.GPIO_PinConfig.GPIO_PinNumber =GPIO_PIN_NO_2;
+	SPIPins.GPIO_PinConfig.GPIO_PinNumber =GPIO_PIN_NO_5;
 	GPIO_Init(&SPIPins);
 
 	//MOSI
-	SPIPins.GPIO_PinConfig.GPIO_PinNumber =GPIO_PIN_NO_6;
+	SPIPins.GPIO_PinConfig.GPIO_PinNumber =GPIO_PIN_NO_7;
 	GPIO_Init(&SPIPins);
 
 
 	//MISO
-	SPIPins.GPIO_PinConfig.GPIO_PinNumber =GPIO_PIN_NO_5;
+	SPIPins.GPIO_PinConfig.GPIO_PinNumber =GPIO_PIN_NO_6;
 	GPIO_Init(&SPIPins);
 
 	//NSS
 	SPIPins.GPIO_PinConfig.GPIO_PinNumber =GPIO_PIN_NO_4;
 	GPIO_Init(&SPIPins);
 
+}
 
-	void SPI4_Inits(void)
+
+
+	void SPI1_Inits(void)
 	{
 	    //PLL2 CONFIGURATION
 		RCC->PLLCFGR |= (0x7<<4);//PLL2RGE AND VCOSEL AND FRACEN ENABLED
 		RCC->PLL2DIVR &= ~(0X1FF <<0);// CLEARING DIVN2
 		RCC->PLL2DIVR |= (0X4F <<0);//DIVN2 SETTING
-		RCC->PLL2DIVR  |= (0x13 <<16);//DIVQ2 SETTING
+		RCC->PLL2DIVR  |= (0x13 <<12);//DIVP2 SETTING
 
 		RCC->CR |= (1<<26);//PLLON
 
 		while(!(RCC->CR & (1<<27)));//PLL2RDY FLAG.....WAITING FOR THE FLAG
 
-		RCC->D2CCIP1R |= (0X1<<16);	//CK SELECTION- SELECTING PLL2_Q
+		RCC->D2CCIP1R |= (0X1<<12);	//CK SELECTION- SELECTING PLL2_P
 
 
-		SPI_Handle_t SPI4handle;
-		SPI4handle.pSPIx = SPI4;
 
-		SPI_PeriClockControl(SPI4, ENABLE);
+		SPI1handle.pSPIx = SPI1;
 
-		SPI4_GPIOInits();
-		SPI4handle.SPIConfig.SPI_BusConfig= SPI_BUS_CONFIG_SIMPLEX_TXONLY;
-		SPI4handle.SPIConfig.SPI_DeviceMode=SPI_DEVICE_MODE_MASTER;
-		SPI4handle.SPIConfig.SPI_SclkSpeed=SPI_SCLK_SPEED_DIV8;//GENERATES SERIAL CLOCK OF 2MHz
-		SPI4handle.SPIConfig.SPI_DFF= SPI_DFF_8BITS;
-		SPI4handle.SPIConfig.SPI_CPOL= SPI_CPOL_LOW;
-		SPI4handle.SPIConfig.SPI_CPHA= SPI_CPHA_LOW;
-		SPI4handle.SPIConfig.SPI_SSM= SPI_SSM_HW;//HARDWARE SLAVE MANAGEMENT ENABLED FOR NSS PIN
+		SPI_PeriClockControl(SPI1, ENABLE);
 
-		SPI_Init(&SPI4handle);
+		SPI1_GPIOInits();
+		SPI1handle.SPIConfig.SPI_BusConfig= SPI_BUS_CONFIG_SIMPLEX_RXONLY;
+		SPI1handle.SPIConfig.SPI_DeviceMode=SPI_DEVICE_MODE_MASTER;
+		SPI1handle.SPIConfig.SPI_SclkSpeed=SPI_SCLK_SPEED_DIV8;//GENERATES SERIAL CLOCK OF 2MHz
+		SPI1handle.SPIConfig.SPI_DFF= SPI_DFF_8BITS;
+		SPI1handle.SPIConfig.SPI_CPOL= SPI_CPOL_LOW;
+		SPI1handle.SPIConfig.SPI_CPHA= SPI_CPHA_LOW;
+		SPI1handle.SPIConfig.SPI_SSM= SPI_SSM_HW;//HARDWARE SLAVE MANAGEMENT ENABLED FOR NSS PIN
+
+		SPI_Init(&SPI1handle);
 		//APB2 CLOCK HAS BEEN ENABLED IN THE SPI_INIT IMPLEMENTATION
 	}
 
@@ -132,97 +135,90 @@ void Slave_GPIO_InterruptPinInit(void)
 }
 
 
+
+
 int main(void)
 {
-	uint8_t dummy= 0xff;
-
-	Slave_GPIO_InterruptPinInit();
-
-	//this function is used to initialize teh GPIO pins to behave as SPI4 pins
-	SPI4_GPIOInits();
-
-	//this function is used to initialize teh SPI PERIPHERAL PARAMETERS
-	SPI4_Inits();
 
 
 
+    uint8_t dummy = 0xFF;
 
-	/*
-	 * making SSOE 1 does NSS output enable
-	 * the NSS pin is automatically managed by hardware
-	 * i.e when SPE=1, NSS will be pulled to low
-	 * and NSS pin will be high when SPE=0
-	 */
-	SPI_SSOEConfig(SPI4, ENABLE);
+    Slave_GPIO_InterruptPinInit();
+    SPI1_GPIOInits();
+    SPI1_Inits();
+    SPI_IRQInterruptConfig(IRQ_NO_SPI1, ENABLE);
 
-	SPI_IRQInterruptConfig(IRQ_NO_SPI4,ENABLE);
+    while(1)
+    {
+        rcvStop = 0;
 
-		while(1){
+        // Wait for Arduino to notify data ready
+        while(!dataAvailable);
 
-			rcvStop = 0;
+        GPIO_IRQInterruptConfig(IRQ_NO_EXTI9_5, DISABLE);
 
-			while(!dataAvailable); //wait till data available interrupt from transmitter device(slave)
-
-			GPIO_IRQInterruptConfig(IRQ_NO_EXTI9_5,DISABLE);
-
-			//enable the SPI2 peripheral
-			SPI_PeripheralControl(SPI4,ENABLE);
+//        SPI_ReceiveDataIT(&SPI1handle, RcvBuff,20);
 
 
-			while(!rcvStop)
-			{
-				/* fetch the data from the SPI peripheral byte by byte in interrupt mode */
-				while ( SPI_SendDataIT(&SPI4handle,&dummy,1) == SPI_BUSY_IN_TX);
-				while ( SPI_ReceiveDataIT(&SPI4handle,&ReadByte,1) == SPI_BUSY_IN_RX );
-			}
+        // Wait until full message received
+        while(!rcvStop){
+            // Start SPI receive for first byte (ISR handles rest)
+            SPI_ReceiveDataIT(&SPI1handle, RcvBuff+i,1);
+            while(!ready);
+            ready=0;
 
+            delay();
+        }
 
-			// confirm SPI is not busy
-			while( SPI_GetFlagStatus(SPI4,SPI_BUSY_FLAG) );
+        GPIO_IRQInterruptConfig(IRQ_NO_EXTI9_5, ENABLE);
+        dataAvailable = 0;
 
-			//Disable the SPI2 peripheral
-			SPI_PeripheralControl(SPI4,DISABLE);
-
-			printf("Rcvd data = %s\n",RcvBuff);
-
-			dataAvailable = 0;
-
-			GPIO_IRQInterruptConfig(IRQ_NO_EXTI9_5,ENABLE);
-		}
-
-			return 0;
-
+        //initialise_monitor_handles();
+        printf("Received message: %s\n", RcvBuff);
+    }
 }
-
 
 /* Runs when a data byte is received from the peripheral over SPI*/
-void SPI4_IRQHandler(void)
+void SPI1_IRQHandler(void)
 {
+	SPI_IRQHandling(&SPI1handle);
+    if(++i==20){
+    	rcvStop = 1;
+    }
 
-	SPI_IRQHandling(&SPI4handle);
 }
 
 
 
-void SPI_ApplicationEventCallback(SPI_Handle_t *pSPIHandle,uint8_t AppEv)
-{
-	static uint32_t i = 0;
-	/* In the RX complete event , copy data in to rcv buffer . '\0' indicates end of message(rcvStop = 1) */
-	if(AppEv == SPI_EVENT_RX_CMPLT)
-	{
-				RcvBuff[i++] = ReadByte;
-				if(ReadByte == '\0' || ( i == MAX_LEN)){
-					rcvStop = 1;
-					RcvBuff[i-1] = '\0';
-					i = 0;
-				}
-	}
 
+
+void SPI_ApplicationEventCallback(SPI_Handle_t *pSPIHandle, uint8_t AppEv)
+{
+
+
+    if(AppEv == SPI_EVENT_RX_1BYTE) // single byte received
+    {
+     RcvBuff[i++] = ReadByte;
+
+        // End of message detection
+        if(ReadByte == '\0' || i == MAX_LEN)
+        {
+            rcvStop = 1;
+//            RcvBuff[i-1] = '\0'; // terminate string
+            i = 0;
+
+            // Close SPI reception
+            SPI_CloseReception(pSPIHandle);
+        }
+    }
 }
+
 
 /* Slave data available interrupt handler */
 void EXTI9_5_IRQHandler(void)
 {
 	GPIO_IRQHandling(GPIO_PIN_NO_6);
 	dataAvailable = 1;
+
 }
